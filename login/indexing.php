b2f0d4bb32a7bcbeb5f3b2a509b73664846b0f80
TulelaKamati <tulelakamati245@gmail.com>
2025-10-19 21:28:04 +0200

    Update indexing.php

--- login/indexing.php
+++ login/indexing.php
@@ -5,17 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NUST Timetable Manager</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/a81368914c.js" crossorigin="anonymous"></script>
 </head>
 <body>
-
     <header>
         <div class="logo-container">
             <img src="images/Campusconnect_nobg.png" class="logo" alt="Campus Connect Logo">
         </div>
         <div class="buttons">
-            <button type="button" class="btn" id="loginBtn">Login</button>
-            <button type="button" class="btn" id="registerBtn">Register</button>
+            <button type="submit" class="btn" id="loginBtn">Login</button>
+            <button type="submit" class="btn" id="registerBtn">Register</button>
         </div>
     </header>
     
@@ -32,8 +30,9 @@
             <p>CAMPUS CONNECT continues to be a leading educational tool in Namibia, with over 2,000 students registered across various educational institutions. The website has implemented a new digital timetable system to streamline academic scheduling and improve student experience. This system has reduced scheduling conflicts by 85% and improved resource allocation across various university facilities.</p>
         </div>
     </div>
-    
+
     <div class="container">
+        <!--Login form-->
         <div class="form-box login">
             <form action="login_registration.php" method="POST"> 
                 <h1>Login</h1>
@@ -48,7 +47,7 @@
                 <button type="submit" class="btn" name="login">Login</button> 
             </form>
         </div>
-
+        <!--Registration Form-->  
         <div class="form-box register">
             <form action="login_registration.php" method="POST"> 
                 <h1>Registration</h1>
@@ -71,13 +70,14 @@
                 <button type="submit" class="btn" name="register">Register</button> 
             </form>
         </div>
-
         <div class="toggle-box">
+            <!--Toggle box Left-->
             <div class="toggle-panel toggle-left">
                 <h1>Hello, Welcome!</h1>
                 <p>Don't have an Account?</p>
                 <button class="btn register-btn">Register</button>
             </div>
+            <!--Toggle box right-->
             <div class="toggle-panel toggle-right">
                 <h1>Welcome Back!</h1>
                 <p>Already have an Account?</p>
@@ -85,7 +85,7 @@
             </div>
         </div>
     </div>
-
+    
     <footer>
         <div class="footer-container">
             <div>
@@ -110,7 +110,7 @@
             <p>&copy; 2025 Campus Connect Timetable Manager. All rights reserved.</p>
         </div>
     </footer>
-
+    
     <script src="script.js"></script>
 </body>
 </html>
