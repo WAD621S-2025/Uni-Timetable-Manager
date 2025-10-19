5a0e00e712d7f8a0828b0a35ce667b6f43f7d177
TulelaKamati <tulelakamati245@gmail.com>
2025-10-19 14:36:29 +0200

    fixing connection db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $dbname = "campus_connect";
 $username = "root";
 $password = ""; 
-$conn = new mysqli($host ,$dbname ,$username ,$password);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 if($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
